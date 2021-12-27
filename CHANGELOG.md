b02f859d42a524fe03e738d0d5ef4c9117b3ceb6
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-28 00:03:37 +0900

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+# Release Note
+
 ## 1.0.0
 
-- Initial version.
+- First Release!
--- test/improvement_rate_test.dart
+++ test/improvement_rate_test.dart
@@ -2,4 +2,62 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-void main() {}
+import 'package:improvement_rate/improvement_rate.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  final calculator = Calculator.instance;
+
+  group('Test should be decreased.', () {
+    test('Test worse case.', () {
+      final result = calculator.evaluate(before: 35, after: 87);
+
+      expect(result.before, 35);
+      expect(result.after, 87);
+      expect(result.value.toStringAsFixed(3), '-148.571');
+      expect(result.trend, Trend.worse);
+      expect(result.policy, ShouldBe.decreased);
+    });
+
+    test('Test better case.', () {
+      final result = calculator.evaluate(before: 35, after: 10);
+
+      expect(result.before, 35);
+      expect(result.after, 10);
+      expect(result.value.toStringAsFixed(3), '71.429');
+      expect(result.trend, Trend.better);
+      expect(result.policy, ShouldBe.decreased);
+    });
+  });
+
+  group('Test should be increased.', () {
+    test('Test worse case.', () {
+      final result = calculator.evaluate(
+        before: 35,
+        after: 87,
+        policy: ShouldBe.increased,
+      );
+
+      expect(result.before, 35);
+      expect(result.after, 87);
+      expect(result.value.toStringAsFixed(3), '148.571');
+      expect(result.trend, Trend.better);
+      expect(result.policy, ShouldBe.increased);
+    });
+
+    test('Test better case.', () {
+      final result = calculator.evaluate(
+        before: 35,
+        after: 10,
+        policy: ShouldBe.increased,
+      );
+
+      expect(result.before, 35);
+      expect(result.after, 10);
+      expect(result.value.toStringAsFixed(3), '-71.429');
+      expect(result.trend, Trend.worse);
+      expect(result.policy, ShouldBe.increased);
+    });
+  });
+}
